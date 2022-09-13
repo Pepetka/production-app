61886b4fb5c6b1838df0999154f54aa9088b2ead
Vlad Kuznetsov <vlad.kznetsov@gmail.com>
2022-09-13 12:09:16 +0300

    add routing and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { Counter } from "./components/Counter"
+import { Suspense } from "react"
+import { NavLink, Route, Routes } from "react-router-dom"
 import "./index.scss"
+import { CounterPageLazy } from "./pages/Counter/CounterPage.lazy"
+import { HomePageLazy } from "./pages/Home/HomePage.lazy"
 
 const App = () => {
 	return (
 		<div className='App'>
-			<Counter />
+			<ul>
+				<li>
+					<NavLink to={"/"}>Home</NavLink>
+				</li>
+				<li>
+					<NavLink to={"/counter"}>Counter</NavLink>
+				</li>
+			</ul>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path='/' element={<HomePageLazy />} />
+					<Route path='/counter' element={<CounterPageLazy />} />
+				</Routes>
+			</Suspense>
 		</div>
 	)
 }
