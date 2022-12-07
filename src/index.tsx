7d0d8d4f430ac8469d71e667f1e60ff52cdca873
Vlad Kuznetsov <vlad.kznetsov@gmail.com>
2022-12-07 14:42:21 +0300

    add react strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { Suspense } from 'react';
+import { StrictMode, Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@/app/provider/Theme';
 import { ErrorBoundary } from '@/app/provider/ErrorBoundary';
@@ -10,18 +10,18 @@ import App from '@/app/App';
 import '@/app/styles/index.scss';
 import '@/shared/config/i18n/i18nConfig';
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(
-	<Suspense fallback={<PageLoader />}>
-		<BrowserRouter>
-			<StoreProvider>
-				<ThemeProvider>
-					<ErrorBoundary>
-						<App />
-					</ErrorBoundary>
-				</ThemeProvider>
-			</StoreProvider>
-		</BrowserRouter>
-	</Suspense>,
+createRoot(document.getElementById('root') as HTMLElement).render(
+	<StrictMode>
+		<Suspense fallback={<PageLoader />}>
+			<BrowserRouter>
+				<StoreProvider>
+					<ThemeProvider>
+						<ErrorBoundary>
+							<App />
+						</ErrorBoundary>
+					</ThemeProvider>
+				</StoreProvider>
+			</BrowserRouter>
+		</Suspense>
+	</StrictMode>,
 );
