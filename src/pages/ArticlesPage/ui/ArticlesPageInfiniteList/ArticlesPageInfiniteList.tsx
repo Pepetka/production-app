b17df854e632d8414eefed5cc7411bfa2b3a8b41
Vlad Kuznetsov <vlad.kznetsov@gmail.com>
2023-03-05 12:02:30 +0300

    fix ArticlesPageInfiniteList

--- src/pages/ArticlesPage/ui/ArticlesPageInfiniteList/ArticlesPageInfiniteList.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPageInfiniteList/ArticlesPageInfiniteList.tsx
@@ -1,7 +1,12 @@
 import { memo, MutableRefObject, useCallback } from 'react';
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import { ArticlesList } from '@/entities/Article';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { Button, ButtonTheme } from '@/shared/ui/Button';
+import { Text } from '@/shared/ui/Text';
+import { HStack } from '@/shared/ui/Stack';
+import { getArticlesPageHasMore } from '../../model/selectors/getArticlesPageHasMore/getArticlesPageHasMore';
 import { getArticlesPageView } from '../../model/selectors/getArticlesPageView/getArticlesPageView';
 import { getArticlesPageLoading } from '../../model/selectors/getArticlesPageLoading/getArticlesPageLoading';
 import { getArticlesPageError } from '../../model/selectors/getArticlesPageError/getArticlesPageError';
@@ -15,10 +20,12 @@ interface ArticlesPageInfiniteListProps {
 
 export const ArticlesPageInfiniteList = memo(({ wrapperRef }: ArticlesPageInfiniteListProps) => {
 	const dispatch = useAppDispatch();
+	const { t } = useTranslation('articles');
 	const view = useSelector(getArticlesPageView);
 	const limit = useSelector(getArticlesPageLimit);
 	const loading = useSelector(getArticlesPageLoading);
 	const error = useSelector(getArticlesPageError);
+	const hasMore = useSelector(getArticlesPageHasMore);
 	const articles = useSelector(getArticles.selectAll);
 
 	const onScrollEnd = useCallback(() => {
@@ -26,6 +33,15 @@ export const ArticlesPageInfiniteList = memo(({ wrapperRef }: ArticlesPageInfini
 	}, [dispatch]);
 
 	return (
-		<ArticlesList error={error} loading={loading} view={view} articles={articles} onScrollEnd={onScrollEnd} wrapperRef={wrapperRef} limit={limit} />
+		<>
+			<ArticlesList error={error} loading={loading} view={view} articles={articles} onScrollEnd={onScrollEnd} wrapperRef={wrapperRef} limit={limit} />
+			{hasMore && (
+				<HStack w100 justify="center">
+					<Button onClick={onScrollEnd} theme={ButtonTheme.OUTLINE_PRIMARY} disabled={loading}>
+						{loading ? <Text text={t('Loading')} /> : <Text text={t('Load more')} />}
+					</Button>
+				</HStack>
+			)}
+		</>
 	);
 });
